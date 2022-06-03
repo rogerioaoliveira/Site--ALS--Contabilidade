595c0473c3ebe036e02ba436d78c098a0cd3f5ea
Rogério A. Oliveira <rogerioalexandredeoliveira87@gmail.com>
2022-06-03 14:31:55 -0300

    added animation h1

---
+++ Scripts/text-animation.js
@@ -0,0 +1,16 @@
+var i = 0;
+var tag = document.getElementById("line");
+var html = document.getElementById("line").innerHTML;
+var attr = tag.setAttribute("data", html);
+var txt = tag.getAttribute("data");
+var speed = 20;
+
+function typeWriter() {
+    if (i <= txt.length) {
+    document.getElementById("line").innerHTML = txt.slice(0 , i + 1);
+    i++;
+    setTimeout(typeWriter, speed);
+    }
+}
+
+typeWriter();
