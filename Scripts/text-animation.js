e54ec91739edbd131644d1158f6283c61178e282
Rogério A. Oliveira <rogerioalexandredeoliveira87@gmail.com>
2022-11-01 15:36:17 -0300

    bug animation fixed

--- Scripts/text-animation.js
+++ Scripts/text-animation.js
@@ -3,7 +3,7 @@ var tag = document.getElementById("line");
 var html = document.getElementById("line").innerHTML;
 var attr = tag.setAttribute("data", html);
 var txt = tag.getAttribute("data");
-var speed = 20;
+var speed = 60;
 
 function typeWriter() {
     if (i <= txt.length) {
